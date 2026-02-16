5af166bc9edc43b99298b875def15120bf587d18
perlin-cyber-god <perlin25101@iiitnr.edu.in>
2026-02-16 08:31:09 +0530

    Fix overlay issue - make subjects topbar scrollable with content

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,23 +13,23 @@ interface LayoutProps {
 
 export const Layout: React.FC<LayoutProps> = ({ children, weather = 'snow', level = 0, progress = 0, topbar }) => {
   return (
-    <div className="min-h-screen bg-mc-bg text-gray-200 font-pixel selection:bg-mc-green selection:text-white relative overflow-hidden flex flex-col">
+    <div className="min-h-screen bg-mc-bg text-gray-200 font-pixel selection:bg-mc-green selection:text-white relative overflow-hidden flex flex-col pt-20">
       <CustomCursor />
       
       <WeatherEffects effectType={weather} particleCount={80} />
 
       {/* Content Area */}
-      <div className="flex flex-col flex-1 pt-24 overflow-hidden relative z-10">
+      <div className="flex flex-col flex-1 overflow-hidden relative z-10">
         
-        {/* Topbar (Sticky below header) */}
-        {topbar && (
-            <div className="sticky top-0 z-30 w-full">
-                {topbar}
-            </div>
-        )}
-
         {/* Main Scrollable Content */}
         <main className="flex-1 overflow-y-auto w-full">
+             {/* Topbar (Scrolls with content) */}
+             {topbar && (
+                 <div className="w-full sticky top-0 z-30">
+                     {topbar}
+                 </div>
+             )}
+             
              <div className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
                 {children}
              </div>
